c3c8f7abf9cf190a35dc1e365aa361c9c78022e9
Asad <asadkhakimov05gmail.com>
2023-06-02 17:21:17 +0600

    add Login Agents

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.scss';
 import Header from "./companents/Header";
 import Footer from "./companents/Footer";
+import Agents from "./companents/Agents/agents";
+import Login from "./companents/Login/login";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-        <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            {/* <Agents/> */}
+            <Login/>
+            <Footer/>
+
+        </div>
+    );
 }
 
 export default App;
